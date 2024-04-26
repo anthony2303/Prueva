85e7a3803936944337a248ec4b0ad212e988c388
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-04-26 16:34:07 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `𝙉𝙀𝙆𝙊𝙏𝙄𝙉𝘼`
+global.authFile = `MichiBot`
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile)
 const msgRetryCounterMap = (MessageRetryMap) => { }
 const msgRetryCounterCache = new NodeCache()
@@ -195,7 +195,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['Nekotina, 'Edge', '2.0.0'] : methodCodeQR ? ['Nekotina', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
+browser: opcion == '1' ? ['ElviraBot', 'Edge', '2.0.0'] : methodCodeQR ? ['ElviraBot', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
