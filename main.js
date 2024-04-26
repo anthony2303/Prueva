6eb565f95eb6bdfbc850edab289f306222eaa115
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-04-26 13:37:14 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['ElviraBot', 'Edge', '2.0.0'] : methodCodeQR ? ['ElviraBot', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
+browser: opcion == '1' ? ['Nekotina, 'Edge', '2.0.0'] : methodCodeQR ? ['Nekotina', 'Edge', '2.0.0'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
