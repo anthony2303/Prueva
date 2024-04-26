e4d4e60f936645f9c1e81a8a59ccc7c3582f4837
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-04-26 13:43:22 -0600

    Actualizar menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -105,7 +105,7 @@ let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.ge
 ⊜ *Pasatiempo(s)* ➺ ${user.pasatiempo === 0 ? '*Sin Registro*' : user.pasatiempo + '\n'}
 ⊜ *Experiencia ➟* ${exp} ⚡
 ⊜ *Diamantes ➟* ${limit} 💎
-⊜ *ElviraCoins ➟* ${money} 🐈
+⊜ *NekoCoins ➟* ${money} 🐈
 ⊜ *Tokens ➟* ${joincount} 🪙
 
 *╭━〔 🐈 OPCIONES DE MENU 🐈 〕⬣*
